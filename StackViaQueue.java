9393f3d56fc1fb99440305efe13588465abc1c93
Michael Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2023-03-04 10:43:28 -0500

    Modified peek method

--- StackViaQueue.java
+++ StackViaQueue.java
@@ -24,13 +24,39 @@ public class StackViaQueue<E> implements Stack<E> {
     }
 
     public E peek() {
-        // TODO
-        return queue.peek();
+
+        E toReturn = null;
+
+        if (queue.size() != 0) {
+            for (int i = 0; i < queue.size(); i++) {
+                queue.offer(queue.poll());
+
+                if (i == queue.size() - 2) {
+                    toReturn = queue.peek();
+                }
+            }
+        }
+
+        return toReturn;
     }
 
     public void push(E item) {
         // TODO
         queue.offer(item);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new StackViaQueue<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
     }
 
 }
