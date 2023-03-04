747c76f95be9405b95a62ba773084304fad34305
Michael Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2023-03-04 10:47:14 -0500

    Crosschecked

--- StackViaQueue.java
+++ StackViaQueue.java
@@ -45,18 +45,4 @@ public class StackViaQueue<E> implements Stack<E> {
         queue.offer(item);
     }
 
-    public static void main(String[] args) {
-        Stack<Integer> stack = new StackViaQueue<>();
-
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-    }
-
 }
